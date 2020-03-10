8b0bba58350400a68afa1ed6ceca89d8080fad67
fenrilius <52090483+fenrilius@users.noreply.github.com>
2020-03-10 21:45:10 +0700

    Add files via upload

---
+++ Instagram/koneksi.php
@@ -0,0 +1,4 @@
+<?php
+	$conn = mysqli_connect("localhost","root","",
+    "dbinstagram") or die ("Koneksi gagal!");
+?>
---
+++ Instagram/login.php
@@ -0,0 +1,30 @@
+<?php
+include "koneksi.php";
+
+session_start();
+
+//dapetin data index.html
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//nyocokin + cek ke database
+$data = mysqli_query($conn,"select * from user where username='$username' and password='$password'");
+$cek = mysqli_num_rows($data);
+ 
+if($cek > 0){
+	$data_profil = mysqli_query($conn,"select * from profile where username='$username'");
+	$row_akun = mysqli_fetch_array($data_profil);
+	$_SESSION['username'] = $username;
+	$_SESSION['status'] = "login";
+	$_SESSION["name"] = $row_akun["name"];
+	$_SESSION["website"] = $row_akun["website"];
+	$_SESSION["bio"] = $row_akun["bio"];
+	$_SESSION["email"] = $row_akun["email"];
+	$_SESSION["nohp"] = $row_akun["nohp"];
+	$_SESSION["gender"] = $row_akun["gender"];
+	header("location:feed.php");
+}else{
+	header("location:index.php");
+
+}
+?>
